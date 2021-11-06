d5d887a05bfda76fa5aea2ccc96c6f96cf7ad6b0
“Ivan <ivan.rivera@casper.com>
2021-11-06 08:53:27 -0500

    add subcomponents routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home/home.page';
+import { TrackMatchComponent } from './track-match/track-match.component';
+import { AboutComponent } from './about/about.component';
+import { TeamsListComponent } from './teams-list/teams-list.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomePage
+  },
+  {
+    path: 'track-match',
+    component: TrackMatchComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'teams-list',
+    component: TeamsListComponent
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HomePage } from './home/home.page';
+import { TrackMatchComponent } from './track-match/track-match.component';
+import { TeamsListComponent } from './teams-list/teams-list.component';
+import { AboutComponent } from './about/about.component';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HomePage],
+  declarations: [
+    AppComponent,
+    HomePage,
+    TrackMatchComponent,
+    TeamsListComponent,
+    AboutComponent,
+  ],
   entryComponents: [],
   imports: [
     BrowserModule,
